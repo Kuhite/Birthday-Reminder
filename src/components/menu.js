3b759188e325702d95942aac168da2e01f320d2e
Aayush Amar Kuhite <aayush.kuhite2@gmail.com>
2021-11-17 16:28:09 +0530

    =Minor Changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -8,6 +8,7 @@ const Menu = () => {
 
     const HandleClick = () => {
         const birthmonth = inputcontainer.current.value;
+        console.log(birthmonth)
         const newarr = Birthday.filter((x) => x.Month === birthmonth);
         console.log(newarr);
 
@@ -24,7 +25,21 @@ const Menu = () => {
         <div>
 
             <div className='form'>
-                <input type='text' ref ={inputcontainer}/>
+                <select name="Month" id="Months" ref ={inputcontainer}>
+                <option value="January">Jan</option>
+                <option value="February">Feb</option>
+                <option value="March">Mar</option>
+                <option value="April">Apr</option>
+                <option value="May">May</option>
+                <option value="June">Jun</option>
+                <option value="July">Jul</option>
+                <option value="August">Aug</option>
+                <option value="September">Sep</option>
+                <option value="October">Oct</option>
+                <option value="November">Nov</option>
+                <option value="December">Dec</option>
+
+                </select>
                 <button type="submit" onClick={HandleClick}>Search</button>
             </div>
             <div className="menu">
